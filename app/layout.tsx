c962339f4003cca1b4d262bc5c16f97c3eea3644
Mcdemzy <adeyemoademola12@gmail.com>
2023-12-29 17:07:29 +0100

    Defined new layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const poppins = Poppins({
 export const metadata: Metadata = {
   title: "Evently",
   description: "Evently is a platform for event management.",
-  icons: "",
+  icons: "/assets/images/logo.svg",
 };
 
 export default function RootLayout({
