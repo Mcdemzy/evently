8774800fff3a9696ca8d22bd270e90b333e8f200
Mcdemzy <adeyemoademola12@gmail.com>
2023-12-31 02:06:54 +0100

    Completed Footer

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,11 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <>
+      <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
+        <div className="wrapper"></div>
+      </section>
+    </>
+  );
 }
