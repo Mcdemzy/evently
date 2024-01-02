9a48b847310dea8cafd7a0bab2ab081658caecc3
Mcdemzy <adeyemoademola12@gmail.com>
2024-01-02 17:41:59 +0100

    Event pages

--- app/(root)/events/create/page.tsx
+++ app/(root)/events/create/page.tsx
@@ -1,4 +1,10 @@
+import EventForm from "@/components/shared/EventForm";
+import { auth } from "@clerk/nextjs";
+
 const CreateEvent = () => {
+  const { sessionClaims } = auth();
+
+  const userId = sessionClaims?.userId as string;
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
@@ -6,7 +12,9 @@ const CreateEvent = () => {
           Create Event
         </h3>
       </section>
-      <div className="wrapper my-8"></div>
+      <div className="wrapper my-8">
+        <EventForm userId={userId} type="Create" />
+      </div>
     </>
   );
 };
