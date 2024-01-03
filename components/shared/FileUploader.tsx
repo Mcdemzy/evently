ab82c85f0118a837277eaf9d71682f636ec12885
Mcdemzy <adeyemoademola12@gmail.com>
2024-01-03 09:58:27 +0100

    Configuring Uploadthing

---
+++ components/shared/FileUploader.tsx
@@ -0,0 +1,16 @@
+import { Dispatch, SetStateAction } from "react";
+
+type FileUploaderProps = {
+  imageUrl: string;
+  onFieldChange: (value: string) => void;
+  setFiles: Dispatch<SetStateAction<File[]>>;
+};
+const FileUploader = ({
+  imageUrl,
+  onFieldChange,
+  setFiles,
+}: FileUploaderProps) => {
+  return <div>FileUploader</div>;
+};
+
+export default FileUploader;
