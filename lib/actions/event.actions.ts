7035f9504cff07a1fc325249180ec189c9b8fc81
Mcdemzy <adeyemoademola12@gmail.com>
2024-01-06 21:27:12 +0100

    Added upload thing domain

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -5,6 +5,17 @@ import { handleError } from "../utils";
 import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
 import Event from "../database/models/event.model";
+import Category from "../database/models/category.model";
+
+const populateEvent = async (query: any) => {
+  return query
+    .populate({
+      path: "organizer",
+      model: User,
+      select: "_id firstName lastName",
+    })
+    .populate({ path: "category", model: Category, select: "_id name" });
+};
 
 export const createEvent = async ({
   event,
@@ -29,7 +40,7 @@ export const createEvent = async ({
     return JSON.parse(JSON.stringify(newEvent));
   } catch (error) {
     console.log(error);
-    // handleError(error);
+    handleError(error);
   }
 };
 
@@ -37,7 +48,7 @@ export const getEventById = async (eventId: string) => {
   try {
     await connectToDatabase();
 
-    const event = await Event.findById(eventId);
+    const event = await populateEvent(Event.findById(eventId));
 
     if (!event) {
       throw new Error("Event not found");
