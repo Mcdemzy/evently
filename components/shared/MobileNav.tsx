d15d956edafc363d9857d47d1666e56f173e0adf
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-04-13 03:18:11 +0100

    Fixing Bugs

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -16,19 +16,19 @@ const MobileNav = () => {
       <Sheet>
         <SheetTrigger className="align-middle">
           <Image
-            src="assets/icons/menu.svg"
+            src="/assets/icons/menu.svg"
+            alt="menu"
             width={24}
             height={24}
-            alt="menu"
             className="cursor-pointer"
           />
         </SheetTrigger>
         <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
           <Image
             src="/assets/images/logo.svg"
-            width={126}
-            height={38}
             alt="logo"
+            width={128}
+            height={38}
           />
           <Separator className="border border-gray-50" />
           <NavItems />
