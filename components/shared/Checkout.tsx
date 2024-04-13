060d544b87fcdd502ab2e519b2d92e464b27e35e
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-04-13 04:26:24 +0100

    Integrating Stripe

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Button } from "../ui/button";
+import { IEvent } from "@/lib/database/models/event.model";
+import { loadStripe } from "@stripe/stripe-js";
 
-const Checkout = () => {
-  return <div>Checkout</div>;
+// Make sure to call `loadStripe` outside of a component’s render to avoid
+// recreating the `Stripe` object on every render.
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY
+);
+
+const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
+  const onCheckout = async () => {
+    console.log("CHECKOUT");
+  };
+  return (
+    <form action={onCheckout} method="post">
+      <Button type="submit" role="link" size="lg" className="button sm:w-fit">
+        {event.isFree ? "Get Tickets" : "Buy Tickets"}
+      </Button>
+    </form>
+  );
 };
 
 export default Checkout;
