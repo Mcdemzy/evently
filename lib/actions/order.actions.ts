5d796f99cbc19a43a4cb0870fca6ce16e41abe6c
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-04-14 04:07:57 +0100

    Connecting to stripe server

---
+++ lib/actions/order.actions.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import Stripe from "stripe";
+import { CheckoutOrderParams } from "@/types";
+
+export const checkoutOrder = async (order: CheckoutOrderParams) => {
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!);
+
+  const price = order.isFree ? 0 : Number(order.price) * 100;
+
+  try {
+    const session = await stripe.checkout.sessions.create({
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            unit_amount: price,
+            product_data: {
+              name: order.eventTitle,
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        eventId: order.eventId,
+        buyerId: order.buyerId,
+      },
+      mode: "payment",
+      success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/?success=true`,
+      cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/?canceled=true`,
+    });
+  } catch (error) {
+    throw error;
+  }
+};
