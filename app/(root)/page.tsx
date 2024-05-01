02f9374b7b7eccac4c8df83dda1014b6e7dca995
Adeyemo Ademola <adeyemoademola12@gmail.com>
2024-05-01 13:05:22 +0100

    Working on Category Filter

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,4 @@
+import CategoryFilter from "@/components/shared/CategoryFilter";
 import Collection from "@/components/shared/Collection";
 import Search from "@/components/shared/Search";
 import { Button } from "@/components/ui/button";
@@ -7,12 +8,14 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default async function Home({ searchParams }: SearchParamProps) {
-  const page = Number;
+  const page = Number(searchParams?.page) || 1;
+  const searchText = (searchParams?.query as string) || "";
+  const category = (searchParams?.category as string) || "";
 
   const events = await getAllEvents({
-    query: "",
-    category: "",
-    page: 1,
+    query: searchText,
+    category,
+    page,
     limit: 6,
   });
 
@@ -50,7 +53,8 @@ export default async function Home({ searchParams }: SearchParamProps) {
           Thousands of Events
         </h2>
         <div className="flex w-full flex-col gap-5 md:flex-">
-          <Search /> CategoryFilter
+          <Search />
+          <CategoryFilter />
         </div>
         <Collection
           data={events?.data}
